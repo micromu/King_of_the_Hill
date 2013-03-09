799e3ecc7977536b29b01981cf5783866d752a0c
micromu-it <sara.gallo@gmail.com>
2013-03-09 16:54:38 +0100

    Hopefully this time I make a commit right... trying to remove the old file that I don't need anymore

--- ARDroneLib/Soft/Build/targets_versions/sdk_PROD_MODE_vlib_Linux_3.2.0-35-generic_GNU_Linux_usrbingcc_4.6.3/ATcodec/ATcodec_Memory.c.d
+++
@@ -1,7 +0,0 @@
-ATcodec_Memory.o ../../Soft/Build/targets_versions/sdk_PROD_MODE_vlib_Linux_3.2.0-35-generic_GNU_Linux_usrbingcc_4.6.3/ATcodec/ATcodec_Memory.c.d: ..//ATcodec/ATcodec_Memory.c ../VP_Os/vp_os_assert.h \
- /usr/include/assert.h /usr/include/features.h \
- /usr/include/x86_64-linux-gnu/bits/predefs.h \
- /usr/include/x86_64-linux-gnu/sys/cdefs.h \
- /usr/include/x86_64-linux-gnu/bits/wordsize.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs-64.h ../ATcodec/ATcodec_Memory.h

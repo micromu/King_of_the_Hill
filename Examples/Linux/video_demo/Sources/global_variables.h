eb090ad07c331801870f78fe0b6c43cd0fa95556
micromu-it <sara.gallo@gmail.com>
2013-03-14 15:56:17 +0100

    prevent the video_recorder thread to start

---
+++ Examples/Linux/video_demo/Sources/global_variables.h
@@ -0,0 +1,40 @@
+//CONSTANTS
+//TODO: find the right values
+#define hill_max_distance 50
+#define hill_min_distance 10
+
+#define enemy_max_distance 50
+#define enemy_min_distance 20
+#define enemy_shooting_distance 30
+
+#define number_of_ammo 5
+
+//DRONE LOGIC
+//These flags are set by the gtk thread
+int game_active = 0;
+int match = 0;
+//These flags are set in the video stream analisys
+int hill_in_sight = 0;
+int enemy_in_sight = 0;
+int hill_distance = 0;
+int enemy_distance = 0;
+int hill_offset_from_center = 0;
+int enemy_offset_from_center = 0;
+int drone_above_hill = 0; //the drone is above the target hill
+int enemy_on_target = 0;//the enemy is in the center of the image
+//These flags are set by the score logic thread
+int drone_wounded = 0;
+int enemy_wounded = 0;
+
+//VIDEO STREAM
+int active_cam = 0; //who set this?
+
+//WIIMOTE
+int drone_in_sight = 0;
+int ammo_in_sight = 0;//1 if there are ammunition in sight to recharge
+int ammunitions_left = 0;
+
+//SCORE LOGIC
+//remember to set to 0/false drone_wounded and enemy_wounded after adding to te total score
+// e se potesse fare punto solo il drone, e il nemico si limitasse a stordirlo per tot secondi?
+
